620c27a67f42191c77db56ba12aa86a437e579ae
Joseph Bosman <joseph.a.bosman@gmail.com>
2026-01-28 13:11:33 -0500

    Setup prettier

--- citr-v9-project/padre-ginos/src/App.js
+++ citr-v9-project/padre-ginos/src/App.js
@@ -1,24 +1,16 @@
 const Pizza = (props) => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, props.name),
-            React.createElement("p", {}, props.description),
-        ]
-    )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Padre Gino's"),
-            React.createElement(Pizza, { name: "Pepperoni", description: "yummy"}),
-            React.createElement(Pizza, { name: "Cheese", description: "yummy2"}),
-        ]
-    )
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, { name: "Pepperoni", description: "yummy" }),
+    React.createElement(Pizza, { name: "Cheese", description: "yummy2" }),
+  ]);
 };
 
 const container = document.getElementById("root");
