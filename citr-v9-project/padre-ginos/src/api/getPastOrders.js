cd157d5bc9f7815973b6364539b0d874d6c06c23
Joe Bosman <joseph.a.bosman@gmail.com>
2026-02-10 11:16:11 -0500

    Setup useQuery on past orders page

--- citr-v9-project/padre-ginos/src/api/getPastOrders.js
+++ citr-v9-project/padre-ginos/src/api/getPastOrders.js
@@ -1,5 +1,5 @@
 export default async function getPastOrders(page){
-  const response = await fetch(`api/past-orders?pages=${page}`);
-  const data = response.json();
+  const response = await fetch(`/api/past-orders?page=${page}`);
+  const data = await response.json();
   return data;
 }
